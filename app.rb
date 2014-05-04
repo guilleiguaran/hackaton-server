ab466be87302635e7e810b28c671cf30371f89e1
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-05-04 13:26:54 -0500

    Remove leading ':'

--- app.rb
+++ app.rb
@@ -4,13 +4,17 @@ require 'nokogiri'
 require 'yajl'
 
 class App < Nancy::Base
+  get "/" do
+    "Pastelitos: 8, Jugos: 12, Almuerzos: 1, Porciones de Pizza:, Sanduches:"
+  end
+
   get '/booths/:id' do
     url = "http://www3.registraduria.gov.co/censo/_censoresultado.php?nCedula=#{params['id']}"
     response = Requests.get(url)
     doc = Nokogiri::HTML(response.body)
     info = {}
-    doc.css("table tr td").each_slice(2){|el| info[el[0].children.children.text] = el[1].children.text.strip }
-    info["error"] = "ID don't found or invalid" if info.empty?
+    doc.css("table tr td").each_slice(2){|el| info[el[0].children.children.text.gsub(":","")] = el[1].children.text.strip }
+    info["error"] = "La cedula no es valida o no puede votar en estas elecciones" if info.empty?
     Yajl::Encoder.encode(info)
   end
 
@@ -19,8 +23,8 @@ class App < Nancy::Base
     response = Requests.get(url)
     doc = Nokogiri::HTML(response.body)
     info = {}
-    doc.css(".t_jurados tr td").each_slice(2){|el| info[el[0].text] = el[1].text.strip }
-    info["error"] = "ID wasn't selected as jury" if info.empty?
+    doc.css(".t_jurados tr td").each_slice(2){|el| info[el[0].text.gsub(":", "")] = el[1].text.strip }
+    info["error"] = "La persona no fue seleccionada como jurado para estas elecciones" if info.empty?
     Yajl::Encoder.encode(info)
   end
 end
